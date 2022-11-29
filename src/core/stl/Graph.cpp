9ccfc3f22c39ac3db08073244b2b274cc919073e
sam gateau <sam.gateau@epicgames.com>
2022-11-29 15:10:44 -0800

    Exploring subgraph

--- src/core/stl/Graph.cpp
+++ src/core/stl/Graph.cpp
@@ -26,23 +26,299 @@
 //
 #include "Graph.h"
 
+#include <set>
+
+#include <iostream>
+
 using namespace core;
 
 
-Graph::NodePointer Graph::createNode(NodeClass* nodeClass) {
-    auto n = std::make_shared<Node>();
+void Graph::Node::log()
+{
+    std::clog << "Node #" << uuid << std::endl;
+    std::clog << "    <" << _type->name << ">" << std::endl;
+    for (auto i : ins)
+    {
+        auto p = _graph->pin(i);
+        std::clog << "    >--" << p->name() << std::endl;
+        for (auto ei : p->edges)
+        {
+            auto e = _graph->_edges[ei];
+            auto s = _graph->pin(e.from);
+            std::clog << "      +--< n#" << s->node->uuid << "." << s->name() << std::endl;
+        }
 
-    n->ins.resize(nodeClass->ins.size());
-    n->outs.resize(nodeClass->outs.size());
-    n->settings.resize(nodeClass->settings.size());
-    n->_schema = nodeClass;
+    }
+    for (auto o : outs)
+    {
+        auto p = _graph->pin(o);
+        std::clog << "    -->" << p->name() << std::endl;
+        for (auto ei : p->edges)
+        {
+            auto e = _graph->_edges[ei];
+            auto d = _graph->pin(e.to);
+            std::clog << "       +--> n#" << d->node->uuid << "." << d->name() << std::endl;
+        }
+
+    }
+}
+
+Graph::NodePointer Graph::createNode(const NodeTypePointer& nodeType) {
+    auto n = std::make_shared<Node>();
+    n->_graph = this;
+
+    for (auto& i : nodeType->in_pins.pins)
+    {
+        Pin p = { n.get(), i.hash(), (Index) _pins.size(), {}};
+        _pins.emplace_back(p);
+        n->ins.push_back(p.uuid);
+    }
+    for (auto& i : nodeType->out_pins.pins)
+    {
+        Pin p = { n.get(), i.hash(), (Index) _pins.size(), {} };
+        _pins.emplace_back(p);
+        n->outs.push_back(p.uuid);
+    }
+
+    n->_type = nodeType;
+
+    n->uuid = _nodes.size();
+
+    _nodes.emplace_back(n);
+
+    return n;
+}
+
+Graph::ID Graph::connect(Node& nFrom, Name& pFrom, Node& nTo, Name& pTo)
+{
+    auto p_from = nFrom.out_pin(pFrom);
+    if (p_from == nullptr)
+        return NULL_ID;
+
+    auto p_to = nTo.in_pin(pTo);
+    if (p_to == nullptr)
+        return NULL_ID;
+
+    Edge e;
+
+    // detect if there is already an edge arriving to destination we could update and reuse
+    if (!p_to->edges.empty())
+    {
+        e.uuid = p_to->edges.front();
+        e = _edges[e.uuid];
+
+        // Remove edge arriving to dest pin from the previous pin.
+        auto& op = _pins[e.from];
+        // op.removeEdgeId(e.uuid);
+        for (auto it = op.edges.begin(); it != op.edges.end(); ++it)
+        {
+            if (*it == e.uuid)
+            {
+                op.edges.erase(it);
+                break;
+            }
+        }
+
+        // now simply update the from pin and the reuse the allocated edge
+        p_from->edges.emplace_back(e.uuid);
+        _edges[e.uuid].from = p_from->uuid;
+        return e.uuid;
+    }
+    
+    // else just add a new edge
+    e.from = p_from->uuid;
+    e.to = p_to->uuid;
+    e.uuid = _edges.size();
+    _edges.emplace_back(e);
+
+    p_to->edges.emplace_back(e.uuid);
+    p_from->edges.emplace_back(e.uuid);
+    return e.uuid;
+}
+
+Graph::IDs Graph::gatherSourceNodes(const Node* n) const
+{
+    bool allInConnected = true;
+    bool allInDisconnected = true;
+    IDs sourceNodes;
+    for (auto opi : n->ins)
+    {
+        const auto& p = _pins[opi];
+        if (p.edges.empty())
+        {
+            allInConnected = false;
+        } else
+        {
+            allInDisconnected = false;
+            const auto& e = _edges[p.edges[0]];
+            const auto& ps = _pins[e.from];
+            sourceNodes.emplace_back(ps.node->uuid);
+        }
+    }
+    return sourceNodes;
+}
+
+void Graph::gatherOuterNodes(const std::vector<IDs>& sourceNodesPerNode, std::set<ID>& nodeReservoirA, std::set<ID>& nodeReservoirB) const
+{
+    // Fill reservoir B with the nodes feeding inputs to some other nodes
+    for (auto a : nodeReservoirA)
+    {
+        for (auto b : sourceNodesPerNode[a])
+        {
+            nodeReservoirB.insert(b);
+        }      
+    }
+
+    // Now remove the inputing nodes from reservoir a
+    for (auto b : nodeReservoirB)
+    {
+        nodeReservoirA.erase(b);
+    }
+
+    // Reservoir A is left with only outer nodes
+}
+
+void Graph::evalInOutPins() const
+{
+    _inPins.clear();
+    _outPins.clear();
+
+    // just grab the ins and outs pins for the full graph
+    for (auto& p : _pins)
+    {
+        if (p.edges.empty())
+        {
+            auto a = p.pinType().access;
+            if (a == PinAccess::in)
+            {
+                _inPins.push_back(p.uuid);
+            } else if (a == PinAccess::out)
+            {
+                _outPins.push_back(p.uuid);
+            }
+        }
+    }
+}
+
+void Graph::evalTraverseOrder() const
+{
+    _traverseOrder.clear();
+    _inNodesCount = 0;
+    _outNodesCount = 0;
+    _nodeWavesCount = 0;
+
+    // Store all the nodes in a set, the starting reservoir
+    std::set<ID> nodeReservoirs[2];
+    for (auto& n : _nodes)
+        nodeReservoirs[0].insert(n->uuid);
+
+    // For every node, the source nodes needed
+    std::vector<IDs> sourceNodesArray;
+    for (auto& n : _nodes)
+        sourceNodesArray.emplace_back(gatherSourceNodes(n.get()));
+
+    // Find the outer nodes from the reservoir
+    // start from the full reservoir, every pass, separate the outers and 
+    // record them in the traverse order.
+    auto pass = 0;
+    auto reservoirIndex = pass % 2;
+    while (nodeReservoirs[reservoirIndex].size())
+    {
+        gatherOuterNodes(sourceNodesArray, nodeReservoirs[reservoirIndex], nodeReservoirs[(pass + 1) % 2]);
+        if (nodeReservoirs[pass].empty())
+            break; /// THis should not happen, means a 
+
+        // Add all the outer nodes in the traverse order
+        for (auto n : nodeReservoirs[reservoirIndex])
+            _traverseOrder.push_back(n);
+
+        // Store the number of outers in this pass
+        _inNodesCount = nodeReservoirs[reservoirIndex].size();
+
+        // How many last outer nodes
+        if (pass == 0) 
+            _outNodesCount = _inNodesCount;
+
+        // next pass start fresh with the B reservoir
+        nodeReservoirs[reservoirIndex].clear();
+        ++pass;
+        reservoirIndex = pass % 2;
+    }
+    _nodeWavesCount = pass;
+    std::reverse(_traverseOrder.begin(), _traverseOrder.end());
+}
+
+void Graph::traverse(std::function<void(Node*, int32_t)> visitor) const
+{
+    evalTraverseOrder();
+
+    int32_t i = 0;
+    for (auto ni : _traverseOrder)
+    {
+        visitor(_nodes[ni].get(), i);
+        ++i;
+    }
+}
+
+void Graph::log() const
+{
+    traverse([](Node* n, int32_t i) {
+            std::clog << "#" << i << std::endl;
+
+            n->log();
+        });
+    std::clog << "Num node passes " << _nodeWavesCount << std::endl;
+    std::clog << "In node count " << _inNodesCount << std::endl;
+    std::clog << "Out node count " << _outNodesCount << std::endl;
+}
+
+Graph::NodeGraphPointer Graph::createNodeGraph(const Graph& subgraph)
+{
+    // Make the node type out of the subgraph
+
+    // duplicate the subgraph
+
 
 }
 
 void Graph::testGraph()
 {
-    auto nc = NodeClass();
-
+    NodeTypePointer nt = std::make_shared<NodeType>(NodeType::Init{
+        "MyFirstType",
+        {ADD_IN(aaa, Scalar)},
+        {ADD_OUT(bbb, Scalar)}
+        });
     
+    NodeTypePointer nt2 = std::make_shared<NodeType>(NodeType::Init{
+        "MySecondType",
+        {ADD_IN(i0, Scalar), ADD_IN(i1, Image)},
+        {ADD_OUT(o0, Scalar), ADD_OUT(o1, Scalar) }
+        });
+    
+    Graph g;
+    auto n0 = g.createNode(nt);
+    
+    auto n1 = g.createNode(nt);
+
+    auto n2 = g.createNode(nt);
+    
+    auto n3 = g.createNode(nt);
+
+    auto n4 = g.createNode(nt2);
+    
+    g.connect(n0, "bbb", n1, "aaa");
+    g.connect(n2, "bbb", n0, "aaa");
+  
+    g.connect(n4, "o0", n2, "aaa");
+    g.connect(n4, "o1", n3, "aaa");
+    
+    g.log();
+
+    Graph g2;
+
+    auto nodeA = g2.createNodeGraph(g);
+
 
 }
+
+
--- src/core/stl/Graph.h
+++ src/core/stl/Graph.h
@@ -30,22 +30,34 @@
 #include <string>
 #include <array>
 #include <memory>
+#include <functional>
+#include <set>
 
-#define ADD_IN(name, t)
+#define STR1(x)  #x
+#define STR(x)  STR1(x)
+#define ADD_IN(name, t) { STR(name), { PinAccess::in, DataType::t }}
+#define ADD_OUT(name, t) { STR(name), { PinAccess::out, DataType::t }}
     
 namespace core {
 
+ //   class Graph;
+ //   using GraphPointer = std::shared_ptr<Graph>;
+
     class Graph {
     public:
-        using id = int32_t;
-        const static id NULL_ID = -1;
+        using Name = const std::string;
+        using Hash = size_t;
 
-        struct Edge {
-            id from = NULL_ID;
-            id to = NULL_ID;
-        };
-        using Edges = std::vector<Edge>;
+        using Index = int32_t;
+        using ID = Index;
+        using NodeID = Index;
+        using PinID = Index;
+        using EdgeID = Index;
+        const static ID NULL_ID = -1;
 
+
+
+        // Describing the Pin Type / Schema
         enum DataType : int8_t
         {
             Scalar = 0,
@@ -60,7 +72,7 @@ namespace core {
 
         };
 
-        enum PinType : int8_t
+        enum PinAccess : int8_t
         {
             in = 0,
             out,
@@ -68,36 +80,204 @@ namespace core {
 
             count
         };
-
-        struct PinClass {
-            const std::string name;
-            PinType           type = in;
+        
+        struct PinType {
+            PinAccess         access = in;
             DataType          dataType = Scalar;
+
+            Hash hash() const { return std::hash<int16_t>{}((int16_t)access + 256 * (int16_t)dataType); }
         };
-        using PinClasses = std::vector<PinClass>;
+        using PinTypes = std::vector<PinType>;
 
-        struct Pin {
-            id _id;
-            Edges   edges;
+        struct NamedPinType
+        {
+            Name name;
+            PinType type;
 
-            bool connectTo(id to) {
-                edges.push_back({ _id, to });
+            static Hash hash(const NamedPinType& npt)
+            {
+                auto v = std::hash<std::string>{}(npt.name);
+                v += npt.type.hash();
+                return std::hash<Hash>{}(v);
+            }
+            Hash hash() const { return hash(*this); }
+        };
+        using NamedPinTypes = std::vector<NamedPinType>;
+
+        
+
+        struct NamedPinTypeSet
+        {
+            NamedPinTypes pins;
+            
+            Index operator[] (Name& name) const
+            {
+                Index i = 0;
+                for (const auto& it : pins)
+                {
+                    if (it.name.compare(name) == 0)
+                        return i;
+
+                    ++i;
+                }
+                return -1;
+            }
+
+            Index operator[] (Hash hash) const
+            {
+                Index i = 0;
+                for (const auto& it : pins)
+                {
+                    if (it.hash() == hash)
+                        return i;
+                    ++i;
+                }
+                return -1;
+            }
+
+            Hash hash() const
+            {
+                Hash v = 0;
+                for (const auto& it : pins)
+                {
+                    v += it.hash();
+                }
+                return std::hash<Hash>{}(v);
             }
         };
+
+        // Describing the Node Type as a name + collection of pin type sets
+        struct NodeType {
+
+            struct Init {
+                Name name;
+                NamedPinTypes ins;
+                NamedPinTypes outs;
+            };
+
+            Name name;
+            NamedPinTypeSet in_pins;
+            NamedPinTypeSet out_pins;
+
+            NodeType(const Init& init) :
+                name(init.name),
+                in_pins({ init.ins }),
+                out_pins({init.outs }) {}
+
+            Hash hash() const
+            {
+                auto v = std::hash<std::string>{}(name);
+                v += in_pins.hash();
+                v += out_pins.hash();
+                return std::hash<Hash>{}(v);
+            };
+
+            auto pin(Hash hash) const {
+                auto i = in_pins[hash];
+                if (i != NULL_ID)
+                    return std::pair(i, PinAccess::in);
+                i = out_pins[hash];
+                if (i != NULL_ID)
+                    return std::pair(i, PinAccess::out);
+
+                return std::pair(NULL_ID, PinAccess::count);
+            }
+
+            Name pinName(Hash hash) const {
+                auto i = pin(hash);
+                if (i.first != NULL_ID)
+                {
+                    if (i.second == PinAccess::in)
+                        return in_pins.pins[i.first].name;
+                    else
+                        return out_pins.pins[i.first].name;
+                }
+                return "";
+            }
+
+            PinType pinType(Hash hash) const {
+                auto i = pin(hash);
+                if (i.first != NULL_ID)
+                {
+                    if (i.second == PinAccess::in)
+                        return in_pins.pins[i.first].type;
+                    else
+                        return out_pins.pins[i.first].type;
+                }
+                return PinType();
+            }
+
+        };
+
+        using IDs = std::vector<ID>;
+        using NodeIDs = std::vector<NodeID>;
+        using PinIDs = std::vector<PinID>;
+        using EdgeIDs = std::vector<EdgeID>;
+
+        class Node;
+        struct Pin {
+            Node*   node = nullptr;
+            Hash    named_type_hash = 0;
+            PinID   uuid = 0;
+
+            EdgeIDs  edges;
+
+            Name    name() const { return node->_type->pinName(named_type_hash); }
+            PinType pinType() const { return node->_type->pinType(named_type_hash); }
+        };
+
         using Pins = std::vector<Pin>;
 
-        struct NodeSchema;
-        using NodeSchemaPointer = std::shared_ptr<NodeSchema>;
+        struct Edge {
+            PinID from = NULL_ID;
+            PinID to = NULL_ID;
+
+            EdgeID uuid = NULL_ID;
+        };
+
+        using Edges = std::vector<Edge>;
+
+
+        using NodeTypePointer = std::shared_ptr<NodeType>;
 
         class Node {
-            NodeSchema* _schema;
+            NodeTypePointer _type;
             
             friend Graph;
-            void setup(NodeSchema* schema);
+            Graph* _graph;
         public:
-            Pins ins;
-            Pins outs;
-            Pins settings;
+          //  const ID operator[] (Name& name) const { return (*const_cast<Node*>(this))[name]; }
+
+            PinID in_pin_id(Name& name) const {
+                auto i = _type->in_pins[name];
+                if (i >= 0)
+                {
+                    return ins[i];
+                }
+                return NULL_ID;
+            }
+            PinID out_pin_id(Name& name) const {
+                auto i = _type->out_pins[name];
+                if (i >= 0)
+                {
+                    return outs[i];
+                }
+                return NULL_ID;
+            }
+
+            Pin* in_pin(Name& name) const {
+                return _graph->pin(in_pin_id(name));
+            }
+            Pin* out_pin(Name& name) const {
+                return _graph->pin(out_pin_id(name));
+            }
+
+
+            PinIDs ins;
+            PinIDs outs;
+            NodeID uuid = NULL_ID;
+
+            void log();
 
         };
         using NodePointer = std::shared_ptr<Node>;
@@ -106,21 +286,55 @@ namespace core {
 
         using NodeFunction = void (*)(Node& n);
 
-        struct NodeSchema {
-            virtual ~NodeSchema() {}
 
-            PinClasses ins;
-            PinClasses outs;
-            PinClasses settings;
-            std::string name;
+        /// Graph methods to build the graph!
+
+        // Create a node
+        NodePointer createNode(const NodeTypePointer& nodeType);
+
+        EdgeID connect(const NodePointer& nodeFrom, Name& pinFrom, const NodePointer& nodeTo, Name& pinTo)
+        {
+            return connect(*nodeFrom, pinFrom, *nodeTo, pinTo);
+        }
+        EdgeID connect(Node& nodeFrom, Name& pinFrom, Node& nodeTo, Name& pinTo);
+        
+        Pin* pin(PinID uuid) { return (uuid == NULL_ID ? nullptr : _pins.data() + uuid); }
+
+
+        class NodeGraph : public Node
+        {
+            Graph _subgraph;
+        public:
+
         };
 
+        using NodeGraphPointer = std::shared_ptr<NodeGraph>;
 
-        NodePointer createNode(NodeSchema* NodeSchema);
+        NodeGraphPointer createNodeGraph(const Graph& subgraph);
 
-        
         static void testGraph();
 
+        void log() const;
+
+
         Nodes _nodes;
+        Pins _pins;
+        Edges _edges;
+
+        mutable IDs _inPins;
+        mutable IDs _outPins;
+        
+        mutable IDs _traverseOrder;
+
+        mutable int32_t _inNodesCount = 0; // number of starting nodes in the traverse order
+        mutable int32_t _outNodesCount = 0; // number of ending nodes in the traverse order
+        mutable int32_t _nodeWavesCount = 0; // number of waves to go through the graph
+
+        NodeIDs gatherSourceNodes(const Node* n) const;
+        void gatherOuterNodes(const std::vector<NodeIDs>& sourceNodesPerNode, std::set<ID>& nodeReservoirA, std::set<ID>& nodeReservoirB) const;
+        void evalTraverseOrder() const;
+        void evalInOutPins() const;
+
+        void traverse(std::function<void (Node*, int32_t)> visitor) const;
     };
 }
